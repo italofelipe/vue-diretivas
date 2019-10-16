bc9718577a3269ff1e42fa694d8823f77f89bc42
italofelipe <felipe.italo@hotmail.com>
2019-10-16 15:29:17 -0300

    4ª Aula - Feito um componente de frutas e duplicado toda a estrutura desse componente para o App.js. Isso foi feito para demonstrar o uso de Mixins em Vue.js.

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,38 @@
       <hr>
       <strong><p>{{cpf | formatarCpf | inverter}}</p></strong>
       <input type="text" :value="cpf | formatarCpf">
+      <hr>
+      <Frutas/>
+      <hr>
 
+      <div>
+          <ul>
+              <li v-for="fruta in frutas" :key="fruta">
+                  {{fruta}}
+              </li>
+          </ul>
+          <input type="text" v-model="fruta" @keydown.enter="add">
+      </div>
   </div>
 </template>
 
 <script>
-
+    import Frutas from './components/Frutas'
 
 export default {
   name: 'app',
+    components: {Frutas},
     data() {
         return {
-            cpf: '06265003119'
+            cpf: '06265003119',
+            fruta: '',
+            frutas: ['Abacaxi', 'Laranja', 'Maracujá']
+        }
+    },
+    methods: {
+        add() {
+            this.frutas.push(this.fruta);
+            this.fruta = '';
         }
     },
     filters: {
---
+++ src/components/Frutas.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="fruta in frutas" :key="fruta">
+                {{fruta}}
+            </li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Frutas",
+        data() {
+            return {
+                fruta: '',
+                frutas: ['Abacaxi', 'Laranja', 'Maracujá']
+            }
+        },
+        methods: {
+            add() {
+                this.frutas.push(this.fruta);
+                this.fruta = '';
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+    li {
+        list-style: none;
+    }
+</style>
